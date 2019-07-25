363d437d44d9b015a201f1638db66257e5f99b5b
张超 <zhangchao042@ke.com>
2019-07-25 17:31:52 +0800

    Initial commit

---
+++ lib/TestDart.dart
@@ -0,0 +1,11 @@
+
+// 定义一个函数
+printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// 程序执行入口
+main() {
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
